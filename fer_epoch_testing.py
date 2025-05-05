5beccef181d18dd2b531e14b1ce6c6c6fddb44d8
Augusto <aescudero@sandiego.edu>
2025-05-05 13:19:27 -0700

    Finished epoch testing. Fixed bug in testing code (if __name__ missing)

--- fer_epoch_testing.py
+++ fer_epoch_testing.py
@@ -2,16 +2,17 @@
 
 from fer_training_program import FERTrainingProgram
 
-print("Running Facial Emotion Recognition Training Program with Various Amounts of Epochs")
+if __name__ == "__main__":
+    print("Running Facial Emotion Recognition Training Program with Various Amounts of Epochs")
 
-epoch_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
+    epoch_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
 
-for epoch_amount in epoch_list:
-    print(f"Training with {epoch_amount} Epochs")
-    model_path = f"fer_{epoch_amount}_epoch.pth"
-    output_file = f"{epoch_amount}_epoch_output.txt"
-    FERTrainingProgram(
-        epochs=epoch_amount, learning_rate=0.001, model_path=model_path, output_file=output_file)
-    print(f"{epoch_amount} Epochs - Training Complete!")
+    for epoch_amount in epoch_list:
+        print(f"Training with {epoch_amount} Epochs")
+        model_path = f"fer_{epoch_amount}_epoch.pth"
+        output_file = f"{epoch_amount}_epoch_output.txt"
+        FERTrainingProgram(
+            epochs=epoch_amount, learning_rate=0.001, model_path=model_path, output_file=output_file)
+        print(f"{epoch_amount} Epochs - Training Complete!")
 
-print("Complete - Facial Emotion Recognition Training Program with Various Amounts of Epochs")
+    print("Complete - Facial Emotion Recognition Training Program with Various Amounts of Epochs")
